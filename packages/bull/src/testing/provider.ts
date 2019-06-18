0432920b6ec39a3a55ece513383a042644564c45
anchan828 <anchan828@gmail.com>
2019-06-18 11:40:43 +0900

    changed classProvider to valueProvider for testing

--- packages/bull/src/testing/provider.ts
+++ packages/bull/src/testing/provider.ts
@@ -1,30 +1,12 @@
-import { Injectable } from '@nestjs/common';
-import { ClassProvider } from '@nestjs/common/interfaces';
-import { Job, JobOptions } from 'bull';
+import { ValueProvider } from '@nestjs/common/interfaces';
 import { BullName } from '../bull.interfaces';
 import { getBullQueueToken } from '../bull.utils';
 export const createTestBullProvider = (name: BullName) => {
+  let counter = 1;
   return {
     provide: getBullQueueToken(name),
-    useClass: BullQueueTestProvider,
-  } as ClassProvider;
+    useValue: {
+      add: async (...args: unknown[]) => ({ id: counter++, ...args }),
+    },
+  } as ValueProvider;
 };
-
-@Injectable()
-export class BullQueueTestProvider {
-  private counter = 1;
-
-  public async add(data: object, opts?: JobOptions): Promise<Job<any>>;
-  public async add(
-    name: string | object,
-    data: object,
-    opts?: JobOptions,
-  ): Promise<Job<any>> {
-    let addData: any = name;
-    if (typeof name === 'string') {
-      addData = data;
-    }
-
-    return { id: this.counter++, data: addData } as Job;
-  }
-}
