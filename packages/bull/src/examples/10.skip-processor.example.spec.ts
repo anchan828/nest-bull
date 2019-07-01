18bf1256017a656f63d1ec7216b93953ef080324
keigo <keigo@unity3d.com>
2019-07-01 13:57:18 +0900

    added test

---
+++ packages/bull/src/examples/10.skip-processor.example.spec.ts
@@ -0,0 +1,71 @@
+import { Injectable, Module } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import { Job, Queue } from 'bull';
+import {
+  BullQueue,
+  BullQueueInject,
+  BullQueueProcess,
+} from '../bull.decorator';
+import { BullModule } from '../bull.module';
+
+@BullQueue()
+export class SkipProcessorExampleBullQueue {
+  @BullQueueProcess({
+    skip: true,
+  })
+  public async process(job: Job): Promise<{ status: string }> {
+    return { status: 'not call' };
+  }
+}
+
+@Injectable()
+export class SkipProcessorExampleService {
+  constructor(
+    @BullQueueInject('SkipProcessorExampleBullQueue')
+    public readonly queue: Queue,
+  ) {}
+}
+
+@Module({
+  providers: [SkipProcessorExampleBullQueue, SkipProcessorExampleService],
+})
+export class SkipProcessorExampleModule {}
+@Module({
+  imports: [
+    BullModule.forRoot({
+      queues: [__filename],
+      extra: {
+        defaultProcessorOptions: {
+          skip: true,
+        },
+      },
+    }),
+    SkipProcessorExampleModule,
+  ],
+})
+export class ApplicationModule {}
+
+describe('10. Skip Processor Example', () => {
+  it('test', async () => {
+    const app = await Test.createTestingModule({
+      imports: [ApplicationModule],
+    }).compile();
+    await app.init();
+    const service = app.get<SkipProcessorExampleService>(
+      SkipProcessorExampleService,
+    );
+    expect(service).toBeDefined();
+    expect(service.queue).toBeDefined();
+    const job = await service.queue.add({ data: 'test' });
+
+    await new Promise(resolve =>
+      setTimeout(() => {
+        resolve();
+      }, 2000),
+    );
+
+    await expect(job.getState()).resolves.toBe('waiting');
+
+    await app.close();
+  });
+});
