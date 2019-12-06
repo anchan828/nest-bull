8fb3a4bf3537b4032d6ae9facb19e626a864ea54
anchan828 <anchan828@gmail.com>
2019-12-06 20:36:00 +0900

    refactor: no longer target.name

--- packages/bullmq/src/bull.decorator.ts
+++ packages/bullmq/src/bull.decorator.ts
@@ -1,12 +1,9 @@
 import { Inject, SetMetadata } from "@nestjs/common";
-import * as deepmerge from "deepmerge";
 import { BULL_WORKER_DECORATOR, BULL_WORKER_PROCESSOR_DECORATOR } from "./bull.constants";
 import { BullWorkerOptions, BullWorkerProcessOptions } from "./bull.interfaces";
 import { getBullQueueToken } from "./bull.utils";
 export function BullWorker(options: BullWorkerOptions): Function {
-  return (target: any): void => {
-    Reflect.defineMetadata(BULL_WORKER_DECORATOR, deepmerge({ name: target.name }, options || {}), target);
-  };
+  return SetMetadata(BULL_WORKER_DECORATOR, options);
 }
 
 export function BullWorkerProcess(options: BullWorkerProcessOptions): Function {
