2d39b849e4c0e6c06f3480db196823067b38fcc8
anchan828 <anchan828@gmail.com>
2019-12-10 16:36:19 +0900

    chore: export interfaces

--- packages/bullmq/src/index.ts
+++ packages/bullmq/src/index.ts
@@ -9,3 +9,11 @@ export {
 export { BullModule } from "./bull.module";
 export { BullService } from "./bull.service";
 export { getBullQueueToken } from "./bull.utils";
+export {
+  BullModuleAsyncOptions,
+  BullModuleOptions,
+  BullModuleOptionsFactory,
+  BullQueueEventsOptions,
+  BullQueueOptions,
+  BullWorkerOptions,
+} from "./interfaces";
